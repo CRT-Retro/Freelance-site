aa729a218565eb76aecc930e17b38b38b15c4085
Radman Hadiloo <radmancodm2022@gmail.com>
2025-09-20 18:31:48 +0330

    update seed.sql

--- seed.sql
+++ seed.sql
@@ -1,19 +1,13 @@
--- Insert freelancers
-INSERT INTO users (username, email, password_hash, role, photo, job_title, location)
-VALUES
-('negin', 'negin@gmail.com', '123456789', 'freelancer', NULL, 'Backend Developer', 'Tehran'),
-('sadra', 'sadra@gmail.com', '123456789', 'freelancer', NULL, 'Data Engineer', 'tehran'),
-('ali', 'ali@gmail.com', '123456789', 'freelancer', NULL, 'Data Analyst', 'tehran');
+PRAGMA foreign_keys = ON;
 
--- Insert employers
-INSERT INTO users (username, email, password_hash, role, photo, job_title, location)
+-- Insert users
+INSERT INTO users (username, email, password_hash, role, job_title, location)
 VALUES
-('sajjad', 'sajjad@gmail.com', '123456789', 'employer', NULL, 'Startup Founder', 'Zanjan'),
-('majid', 'majid@gmail.com', '123456789', 'employer', NULL, 'Project Manager', 'Zanjan');
-
--- Insert admins
-INSERT INTO users (username, email, password_hash, role, photo, job_title, location)
-VALUES
-('radman', 'radman@gmail.com', '123456789', 'admin', NULL, 'Database Admin', 'Zanjan'),
-('ronika', 'ronika@gmail.com', '123456789', 'admin', NULL, 'Backend Admin', 'tehran'),
-('sedigh', 'sedigh@gmail.com', '123456789', 'admin', NULL, 'frontend Admin', 'kerman');
+('negin', 'negin@gmail.com', '123456789', 'freelancer', 'Backend Developer', 'Tehran'),
+('sadra', 'sadra@gmail.com', '123456789', 'freelancer', 'Data Engineer', 'Tehran'),
+('ali', 'ali@gmail.com', '123456789', 'freelancer', 'Data Analyst', 'Tehran'),
+('sajjad', 'sajjad@gmail.com', '123456789', 'employer', 'Startup Founder', 'Zanjan'),
+('majid', 'majid@gmail.com', '123456789', 'employer', 'Project Manager', 'Zanjan'),
+('radman', 'radman@gmail.com', '123456789', 'admin', 'Database Admin', 'Zanjan'),
+('ronika', 'ronika@gmail.com', '123456789', 'admin', 'Backend Admin', 'Tehran'),
+('sedigh', 'sedigh@gmail.com', '123456789', 'admin', 'Frontend Admin', 'Kerman');
