27dc102b501469c7f55b0b8e1a061616f69c20ac
Radman Hadiloo <radmancodm2022@gmail.com>
2025-09-22 11:29:26 +0330

    update seed.sql

--- seed.sql
+++ seed.sql
@@ -5,10 +5,9 @@ VALUES
 ('negin', 'negin@gmail.com', '123456789', 'freelancer', 'Backend Developer', 'Tehran'),
 ('sadra', 'sadra@gmail.com', '123456789', 'freelancer', 'Data Engineer', 'Tehran'),
 ('ali', 'ali@gmail.com', '123456789', 'freelancer', 'Data Analyst', 'Tehran'),
+('mohammad', 'mohammad@gmail.com', '123456789', 'freelancer', 'Data Engineer', 'Tehran'),
 ('sajjad', 'sajjad@gmail.com', '123456789', 'employer', 'Startup Founder', 'Zanjan'),
 ('majid', 'majid@gmail.com', '123456789', 'employer', 'Project Manager', 'Zanjan'),
 ('radman', 'radman@gmail.com', '123456789', 'admin', 'Database Admin', 'Zanjan'),
 ('ronika', 'ronika@gmail.com', '123456789', 'admin', 'Backend Admin', 'Tehran'),
-('sedigh', 'sedigh@gmail.com', '123456789', 'admin', 'Frontend Admin', 'Kerman'),
-('mohammad', 'mohammad@gmail.com', '123456789', 'freelancer', 'Data Engineer', 'Tehran');
-
+('sedigh', 'sedigh@gmail.com', '123456789', 'admin', 'Frontend Admin', 'Kerman');
